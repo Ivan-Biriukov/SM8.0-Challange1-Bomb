bc6be13a3089f1563ab2d02cbf0297d89211a66d
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-09 20:03:24 +0500

    Created Custom Cell
    Add UIElements to Cell

---
+++ SM8.0-Challange1-Bomb/Views/RulesCustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RulesCustomTableViewCell.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by Ислам Пулатов on 8/9/23.
+//
+
+import UIKit
+
+class RulesCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
