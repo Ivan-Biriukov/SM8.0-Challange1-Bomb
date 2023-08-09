14c1e1b993fa719319a8c4a9794a5078e28edc4c
Ivan Biriukov <Grottt0@mail.ru>
2023-08-09 16:27:30 +0300

    Initial Commit

---
+++ SM8.0-Challange1-Bomb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by иван Бирюков on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
