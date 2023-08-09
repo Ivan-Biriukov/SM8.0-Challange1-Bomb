65edb1b69a099c0abd991d786e9fcd1e1da48db4
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-09 19:55:13 +0500

    Create RulesViewController

---
+++ SM8.0-Challange1-Bomb/Controllers/RulesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RulesViewController.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by Ислам Пулатов on 8/9/23.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
