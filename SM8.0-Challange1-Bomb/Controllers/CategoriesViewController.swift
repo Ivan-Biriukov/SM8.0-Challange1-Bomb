791e24b04995785ec34eb9fbf2fc7e83bc8b5d4c
Eremenok Elizaveta <eremeonock.el@yandex.ru>
2023-08-10 13:51:38 +0300

    Commit(2)

---
+++ SM8.0-Challange1-Bomb/Controllers/CategoriesViewController.swift
@@ -0,0 +1,114 @@
+//
+//  CategoriesViewController.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by Elizaveta Eremyonok on 10.08.2023.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    private let cellDataArray : [CategoryesDataModel] = [
+        .init(imageName: K.Images.diference, titleLabel: "О Разном", chechMarkSelected: false),
+        .init(imageName: K.Images.sportAndHobby, titleLabel: "Спорт и Хобби", chechMarkSelected: false),
+        .init(imageName: K.Images.aboutLife, titleLabel: "Про Жизнь", chechMarkSelected: true),
+        .init(imageName: K.Images.fameos, titleLabel: "Знаменитости", chechMarkSelected: true),
+        .init(imageName: K.Images.artAndCinema, titleLabel: "Искусство и Кино", chechMarkSelected: false),
+        .init(imageName: K.Images.nature, titleLabel: "Природа", chechMarkSelected: false)
+    ]
+    
+
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let c = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 178, height: 175)
+        c.backgroundColor = .clear
+        c.showsHorizontalScrollIndicator = false
+        c.translatesAutoresizingMaskIntoConstraints = false
+        return c
+    }()
+    
+    
+
+   let backgroundView: UIImageView = {
+       let view = UIImageView()
+       view.image = UIImage(named: "background")
+       view.contentMode = .scaleToFill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+   }()
+
+
+    override func viewDidLoad() {
+        createCustomNavigationBar(title: "Категории")
+        super.viewDidLoad()
+
+        setupViews()
+        setupConstraints()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CategoryesCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+    }
+    
+    private func setupViews() {
+        view.addSubview(backgroundView)
+        view.addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+
+        ])
+    }
+    
+
+    
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoryesCollectionViewCell
+        
+        let currentCell = cellDataArray[indexPath.row]
+        
+        cell.cellData = currentCell
+        cell.addCheckMark(isOn: currentCell.chechMarkSelected)
+        
+        return cell
+            
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
---
+++ SM8.0-Challange1-Bomb/Models/CategoriesDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoriesDataModel.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by Elizaveta Eremyonok on 10.08.2023.
+//
+
+import Foundation
+
+struct CategoryesDataModel {
+    let imageName : String
+    let titleLabel : String
+    let chechMarkSelected : Bool
+}
