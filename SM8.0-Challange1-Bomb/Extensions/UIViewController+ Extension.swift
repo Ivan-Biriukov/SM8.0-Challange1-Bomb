0ace8a6b21f0df09c88ab2d0df6527faeeefe563
Ivan Biriukov <Grottt0@mail.ru>
2023-08-11 00:43:07 +0300

    Rework method for customize navBar when we want add button there. Now method toake 1 input parameter of type UIButton? If we don't wanna  add target to button -> in parameter just set nil value, otherwise in VC create button and use it like parameter

--- SM8.0-Challange1-Bomb/Extensions/UIViewController+ Extension.swift
+++ SM8.0-Challange1-Bomb/Extensions/UIViewController+ Extension.swift
@@ -31,8 +31,21 @@ extension UIViewController {
         self.navigationItem.titleView = navTitle
     }
     
-    func addButtonToNavBar() {
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "pause.circle"), style:.done, target: self, action: nil)
+    func addButtonToNavBar(_ button: UIButton?) {
+        if let safeButton = button {
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: safeButton)
+        } else {
+            let customView : UIButton = {
+                let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+                btn.setImage(UIImage(systemName: "pause.circle"), for: .normal)
+                btn.tintColor = .black
+                btn.setTitle(nil, for: .normal)
+                btn.contentVerticalAlignment = .fill
+                btn.contentHorizontalAlignment = .fill
+                return btn
+            }()
+            self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: customView)
+        }     
     }
     
     // Function check if UserDefault even exist, true when exist (has value) false when doesnt exist (has no value)
