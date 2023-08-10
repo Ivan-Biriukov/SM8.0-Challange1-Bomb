ffd4dcdf96769dad269671bba58963da4b141818
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-08-10 14:57:57 +0500

    Create CategoriesCollectionView

---
+++ SM8.0-Challange1-Bomb/Views/CategoryCustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCustomCollectionViewCell.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by Ислам Пулатов on 8/9/23.
+//
+
+import UIKit
+
+class CategoryCustomCollectionViewCell: UICollectionViewCell {
+    
+}
