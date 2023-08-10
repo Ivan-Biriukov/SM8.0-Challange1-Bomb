da8ce0094f4d976a9ee7dd47707655f3bfa11055
Eremenok Elizaveta <eremeonock.el@yandex.ru>
2023-08-10 11:48:56 +0300

    creating Categoryes VC UI complete

--- SM8.0-Challange1-Bomb/Controllers/CategoriesViewController.swift
+++ SM8.0-Challange1-Bomb/Controllers/CategoriesViewController.swift
@@ -5,22 +5,34 @@
 //  Created by Elizaveta Eremyonok on 09.08.2023.
 //
 
+
 import UIKit
 
-class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    private let cellDataArray : [CategoryesDataModel] = [
+        .init(imageName: K.Images.diference, titleLabel: "О Разном", chechMarkSelected: false),
+        .init(imageName: K.Images.sportAndHobby, titleLabel: "Спорт и Хобби", chechMarkSelected: false),
+        .init(imageName: K.Images.aboutLife, titleLabel: "Про Жизнь", chechMarkSelected: true),
+        .init(imageName: K.Images.fameos, titleLabel: "Знаменитости", chechMarkSelected: true),
+        .init(imageName: K.Images.artAndCinema, titleLabel: "Искусство и Кино", chechMarkSelected: false),
+        .init(imageName: K.Images.nature, titleLabel: "Природа", chechMarkSelected: false)
+    ]
+    
+
     
     let collectionView: UICollectionView = {
-           let layout = UICollectionViewFlowLayout()
-           layout.scrollDirection = .vertical
-           layout.itemSize = CGSize(width: 100, height: 100)
-           let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-           collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-           return collectionView
+        let layout = UICollectionViewFlowLayout()
+        let c = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 178, height: 175)
+        c.backgroundColor = .clear
+        c.showsHorizontalScrollIndicator = false
+        c.translatesAutoresizingMaskIntoConstraints = false
+        return c
     }()
     
-    let reuseIdentifier = "Cell"
+    
 
    let backgroundView: UIImageView = {
        let view = UIImageView()
@@ -40,7 +52,7 @@ class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICo
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(CategoryesCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
     
     private func setupViews() {
@@ -55,9 +67,10 @@ class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICo
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
             collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
 
         ])
     }
@@ -66,86 +79,24 @@ class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICo
     
     // MARK: UICollectionViewDataSource
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CategoryesCollectionViewCell
+        
+        let currentCell = cellDataArray[indexPath.row]
+        
+        cell.cellData = currentCell
+        cell.addCheckMark(isOn: currentCell.chechMarkSelected)
+        
+        return cell
             
-        let imageName: String
-                let title: String
-                switch indexPath.item {
-                    case 0:
-                    imageName = K.Images.diference
-                        title = "О Разном"
-                    case 1:
-                        imageName = "image 2"
-                        title = "Спорт и Хобби"
-                    case 2:
-                        imageName = "image 3"
-                        title = "Про Жизнь"
-                    case 3:
-                        imageName = "image 4"
-                        title = "Знаменитости"
-                    case 4:
-                        imageName = "image 5"
-                        title = "Исскусство и Кино"
-                    case 5:
-                        imageName = "image 10"
-                        title = "Природа"
-                    default:
-                        imageName = "image 10"
-                        title = "По умолчанию"
-                }
-           
-        cell.configure(imageName: imageName, title: title, backgroundColor: .specialViolet)
-                
-                return cell
         }
     }
-
-class CustomCollectionViewCell: UICollectionViewCell {
-    let button: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.cornerRadius = 15
-        button.clipsToBounds = true
-        return button
-    }()
-    
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .center
-        return label
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        contentView.addSubview(button)
-        contentView.addSubview(titleLabel)
-        
-        NSLayoutConstraint.activate([
-            button.topAnchor.constraint(equalTo: contentView.topAnchor),
-            button.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            button.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            button.bottomAnchor.constraint(equalTo: titleLabel.topAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-        ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func configure(imageName: String, title: String, backgroundColor: UIColor) {
-        button.setImage(UIImage(named: imageName), for: .normal)
-        button.backgroundColor = backgroundColor
-        
-        titleLabel.text = title
-    }
-}
---
+++ SM8.0-Challange1-Bomb/Models/CategoryesDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryesDataModel.swift
+//  SM8.0-Challange1-Bomb
+//
+//  Created by Elizaveta Eremyonok on 10.08.2023.
+//
+
+import Foundation
+
+struct CategoryesDataModel {
+    let imageName : String
+    let titleLabel : String
+    let chechMarkSelected : Bool
+}
